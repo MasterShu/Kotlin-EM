8f5b985876fd3a6af9d7508d49a80795b6d613ea
MasterShu <1562688988@qq.com>
2018-03-19 14:32:58 +0800

    init project

---
+++ app/src/main/java/club/mastershu/kotlin_e_mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package club.mastershu.kotlin_e_mall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import club.mastershu.kotlin_e_mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
